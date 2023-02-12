f42a7c80f3304fc510298c85b925ed78f39a64fc
Jakub Bartnicki <bartoonaqera@o2.pl>
2023-02-12 15:15:00 +0100

    replaced print() by logging.info, changed names of payloads and moved them to the beggining

--- test_api_crud.py
+++ test_api_crud.py
@@ -1,20 +1,21 @@
 import requests
 import json
+import logging
 
 def test_get_user():
     p = {"page": 2}
     resp = requests.get("https://reqres.in/api/users", params=p)
     json_response = resp.json()
 
-    print(json_response["total_pages"])
+    logging.info(json_response["total_pages"])
     assert json_response["total_pages"] == 2, "total pages doesn't match"
-    print(json_response["total"])
+    logging.info(json_response["total"])
     assert json_response["total"] == 12, "total doesn't match"
-    print(json_response["data"][0]["email"])
+    logging.info(json_response["data"][0]["email"])
     assert json_response["data"][0]["email"], "invalid email"
-    print(json_response["support"]["url"])
+    logging.info(json_response["support"]["url"])
     assert (json_response["support"]["url"].endswith("heading")), "url wrong"
-    print(json_response["data"][0]["last_name"])
+    logging.info(json_response["data"][0]["last_name"])
     assert json_response["data"][0]["last_name"] != None
 
 def test_get_statuscode():
@@ -29,17 +30,17 @@ def test_create_user():
         "job": "Automation"
     }
 
-    resp = requests.post("https://reqres.in/api/users", data=payload)
-    print(resp.json())
-    assert resp.json()["job"] == "Automation", "wrong data!"
-    assert resp.json()["name"] == "Jan", "wrong data!"
-
-    data2 = {
+    personal_info = {
         "name": "Kuba",
         "job": "Soldier"
     }
 
-    response = requests.post("https://reqres.in/api/users", data=data2)
+    resp = requests.post("https://reqres.in/api/users", data=payload)
+    logging.info(resp.json())
+    assert resp.json()["job"] == "Automation", "wrong data!"
+    assert resp.json()["name"] == "Jan", "wrong data!"
+
+    response = requests.post("https://reqres.in/api/users", data=personal_info)
     print(response.json())
     assert response.json()["job"] == "Soldier", "wrong data!"
     assert response.json()["name"] == "Kuba", "wrong data!"
@@ -47,7 +48,7 @@ def test_create_user():
 def test_create_user_from_file():
     mydata = open("data.json", "r").read()
     resp = requests.post("https://reqres.in/api/users", data=json.loads(mydata))
-    print(resp.json())
+    logging.info(resp.json())
     assert resp.json()["job"] == "Policeman", "wrong data!"
     assert resp.json()["name"] == "Tom", "wrong data!"
 
@@ -55,27 +56,28 @@ def test_put_patch():
 
     #PUT
 
-    mydata = {
+    first_person_data = {
         "name": "Tim",
         "job": "Doctor"
     }
 
-    resp = requests.put("https://reqres.in/api/users/2", data=mydata)
+    second_person_name = {
+        "name": "Tim"
+    }
+
+    resp = requests.put("https://reqres.in/api/users/2", data=first_person_data)
 
     assert resp.status_code == 200
     assert resp.json()["job"] == "Doctor"
-    print(resp.json())
+    logging.info(resp.json())
 
     # PATH
 
-    mydata2 = {
-        "name": "Tim"
-    }
-    response = requests.patch("https://reqres.in/api/users/2", data=mydata2)
+    response = requests.patch("https://reqres.in/api/users/2", data=second_person_name)
     assert response.json()["name"] == "Tim"
-    print(response.json())
+    logging.info(response.json())
 
 def test_delete_user():
     resp = requests.delete("https://reqres.in/api/users/2")
-    print(resp.status_code)
+    logging.info(resp.status_code)
     assert resp.status_code == 204, "User deletion failed"
